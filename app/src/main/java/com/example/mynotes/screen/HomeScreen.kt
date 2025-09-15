47c7613667afb7f91005a5d7493f6349581c7672
palaksinghal14 <palaksinghal148@gmail.com>
2025-09-15 06:19:50 +0530

    added navigation

---
+++ app/src/main/java/com/example/mynotes/screen/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.mynotes.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "This is Home Screen")
+}
